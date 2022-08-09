56b44951aa8ab6f39f067c623f16f50a611d841b
kipraschi <kipraschi@gmail.com>
2022-08-10 00:04:11 +0300

    add hover effect - drawing on the grid

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 for (let i = 0; i < 256; i++) {
 	let square = document.createElement("div");
 	square.classList.add("squares");
+    square.addEventListener('mouseover', () => square.style.backgroundColor = "#000000");
 	document.querySelector("#container").appendChild(square);
-}
+};
